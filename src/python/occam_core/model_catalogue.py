8330a7fe74f6852911b91bef663306838e8c3f85
Mohamed Abdelbary <mo@occami.ai>
2025-01-08 13:11:08 +0000

    Add UserAgentParamsModel to params model catalogue

--- src/python/occam_core/model_catalogue.py
+++ src/python/occam_core/model_catalogue.py
@@ -1,11 +1,14 @@
 from occam_core.agents.util import EmailCommunicatorParamsModel, LLMParamsModel
 from occam_core.util.base_models import IOModel, ParamsIOModel
+from occam_core.agents.model import UserAgentParamsModel
+
 
 # TODO: Auto-generate this by scanning the codebase.
 PARAMS_MODEL_CATALOGUE = {
     ParamsIOModel.__name__: ParamsIOModel,
     LLMParamsModel.__name__: LLMParamsModel,
     EmailCommunicatorParamsModel.__name__: EmailCommunicatorParamsModel,
+    UserAgentParamsModel.__name__: UserAgentParamsModel,
 }
 
 IO_MODEL_CATALOGUE = {
