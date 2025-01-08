7f1ac80dc65f6708575eeeadca78708831677596
amedhatm <ahmed@occam.ai>
2025-01-08 00:58:08 -0500

    remove python. ref

--- src/python/occam_core/model_catalogue.py
+++ src/python/occam_core/model_catalogue.py
@@ -1,6 +1,5 @@
+from occam_core.agents.util import LLMParamsModel
 from occam_core.util.base_models import IOModel, ParamsIOModel
-
-from python.occam_core.agents.util import LLMParamsModel
 
 # TODO: Auto-generate this by scanning the codebase.
 PARAMS_MODEL_CATALOGUE = {
