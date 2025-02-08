8c65e3703015f4cb318c4e6e0e504bd50fc2cb11
amedhatm <ahmed@occam.ai>
2025-02-08 09:37:57 -0500

    update params catalogue

--- src/python/occam_core/agents/params.py
+++ src/python/occam_core/agents/params.py
@@ -1,91 +1,30 @@
-import enum
-from typing import Dict, List, Optional, Type
+from enum import Enum
+from typing import Any, Dict, List, Optional, Type
 
 from occam_core.util.base_models import (AgentInstanceParamsModel,
                                          TAgentInstanceParamsModel)
-from pydantic import BaseModel, model_validator
+from pydantic import BaseModel, Field
+
+"""This file is auto-generated. Do not edit manually."""
 
 
-class CommunicationMethod(str, enum.Enum):
-    SLACK = "slack"
-    EMAIL = "email"
+class ChatPermissions(str, Enum):
+    ANY = "any"
 
 
-class ChatChannelPermission(enum.Enum):
-    READ_ONLY = "read_only"
-    SEND_MESSAGE = "send_message"
-    UPLOAD_FILES = "upload_files"
-    TERMINATE_CHAT = "terminate_chat"
-    ALL = "ALL"
-
-
-# User Provided Human Agent Params
-class UserProvidedHumanAgentParamsModel(AgentInstanceParamsModel):
-    """
-    This is when you need to bring in a human agent into a workflow
-    using agents chat, where the human agent is provided by the user
-    in full.
-    """
-
-    email: str
-    first_name: str
-    last_name: str
-    slack_handle: Optional[str] = None
-    channel_permission: ChatChannelPermission = ChatChannelPermission.READ_ONLY
-    communication_methods: Optional[List[CommunicationMethod]] = None
-
-    @model_validator(mode="after")
-    def check_either_slack_or_email(self):
-        if CommunicationMethod.SLACK in self.communication_methods:
-            assert self.slack_handle, "Slack handle must be provided"
-        return self
-
-
-# Occam Provided Human Agent Params
-class OccamProvidedUserAgentParamsModel(AgentInstanceParamsModel):
-    """
-    This is for occam provided interface agents for which we
-    have contact information and only need to know their designated
-    permissions and communication methods.
-    """
-
-    channel_permission: ChatChannelPermission = ChatChannelPermission.READ_ONLY
-    # which methods are we allowed to reach user through.
-    communication_methods: Optional[List[CommunicationMethod]] = None
-
-    @model_validator(mode="after")
-    def check_communication_methods(self):
-        if self.communication_methods is None:
-            self.communication_methods = [CommunicationMethod.EMAIL]
-        return self
-
-
-# LLM Agent Params
-class LLMAgentParamsModel(AgentInstanceParamsModel):
-    llm_model_name: str
-    system_prompt: Optional[str] = None
-    image_model_name: Optional[str] = None
-    log_chat: Optional[bool] = None
-    assistant_name: Optional[str] = None
-
-
-class DefinedLLMAgentParamsModel(AgentInstanceParamsModel):
-    system_prompt: Optional[str] = None
-    image_model_name: Optional[str] = None
-    log_chat: Optional[bool] = None
-    assistant_name: Optional[str] = None
-
-
-# Communication Agent Params
-class SupervisionType(str, enum.Enum):
+class SupervisionType(str, Enum):
     FULL = "full"
     SELECTIVE = "selective"
     FINAL = "final"
     BATCH = "batch"
 
 
-class EmailCommunicatorCardModel(BaseModel):
+class ChatSelectionRule(str, Enum):
+    ROUND_ROBIN = "ROUND_ROBIN"
+    GENERATIVE = "GENERATIVE"
 
+
+class EmailCommunicatorCardModel(BaseModel):
     email: str
     first_name: str
     last_name: str
@@ -93,44 +32,78 @@ class EmailCommunicatorCardModel(BaseModel):
     role: str
 
 
-class SupervisorCardModel(EmailCommunicatorCardModel):
+class SupervisorCardModel(BaseModel):
+    email: str
+    first_name: str
+    last_name: str
+    company: str
+    role: str
     supervision_type: SupervisionType
 
 
-class EmailCommunicatorAgentParamsModel(AgentInstanceParamsModel):
-    """
-    Parameters for the email communicator tool.
-    """
-
-    goal: str
-    email_communicator_card: EmailCommunicatorCardModel
-    supervisor_card: SupervisorCardModel
 
 
-# Data Structuring Agent Params
+class DefinedLLMAgentParamsModel(AgentInstanceParamsModel):
+    system_prompt: Optional[str] = None
+    log_chat: Optional[bool] = None
+    assistant_name: Optional[str] = None
+    retains_chat_history: bool = False
+    response_format: Optional[type[BaseModel]] = None
+    initial_chat_messages: Optional[list] = None
+
+
+class OccamProvidedUserAgentParamsModel(AgentInstanceParamsModel):
+    session_id: Optional[str] = None
+    chat_permission: ChatPermissions = ChatPermissions.ANY
+
+
+class LLMAgentParamsModel(AgentInstanceParamsModel):
+    system_prompt: Optional[str] = None
+    multimodal_model_name: Optional[str] = None
+    text_model_name: Optional[str] = None
+    log_chat: Optional[bool] = None
+    assistant_name: Optional[str] = None
+    retains_chat_history: bool = False
+    response_format: Optional[type[BaseModel]] = None
+    initial_chat_messages: Optional[list] = None
+
+
 class DataStructuringAgentParamsModel(AgentInstanceParamsModel):
     structuring_goal: str
     structured_output_model: Optional[str] = None
 
 
-# Agents Chat Params
+class EmailCommunicatorAgentParamsModel(AgentInstanceParamsModel):
+    goal: str
+    email_communicator_card: EmailCommunicatorCardModel
+    supervisor_card: SupervisorCardModel
+
+
 class AgentsChatParamsModel(AgentInstanceParamsModel):
-
-    # 1. chat goal
-    chat_goal: str
-    # 2. Agents participating in the chat.
-    agents: Dict[str, TAgentInstanceParamsModel]
-    # 3. Chat session id, this allows merging multiple distinct chat runs together.
+    dollar_budget: float
+    chat_goal: Optional[str] = None
+    agent_selection_rule: ChatSelectionRule = ChatSelectionRule.ROUND_ROBIN
+    agents: Optional[dict[str, TAgentInstanceParamsModel]] = None
+    agent_turn_order: Optional[list[str]] = None
     session_id: Optional[str] = None
+    max_chat_steps: int = 1000
+    chat_manager_name: str = "Occam Chat Assistant"
 
 
-# TODO: Auto-generate this by scanning the codebase.
-PARAMS_MODEL_CATALOGUE = {
-    AgentsChatParamsModel.__name__: AgentsChatParamsModel,
+class InvitedUserAgentParamsModel(AgentInstanceParamsModel):
+    email: str
+    first_name: str
+    last_name: Optional[str] = None
+    session_id: Optional[str] = None
+    chat_permission: ChatPermissions = ChatPermissions.ANY
+
+
+PARAMS_MODEL_CATALOGUE: Dict[str, Type[AgentInstanceParamsModel]] = {
+    DefinedLLMAgentParamsModel.__name__: DefinedLLMAgentParamsModel,
+    OccamProvidedUserAgentParamsModel.__name__: OccamProvidedUserAgentParamsModel,
+    LLMAgentParamsModel.__name__: LLMAgentParamsModel,
     DataStructuringAgentParamsModel.__name__: DataStructuringAgentParamsModel,
     EmailCommunicatorAgentParamsModel.__name__: EmailCommunicatorAgentParamsModel,
-    LLMAgentParamsModel.__name__: LLMAgentParamsModel,
-    UserProvidedHumanAgentParamsModel.__name__: UserProvidedHumanAgentParamsModel,
-    OccamProvidedUserAgentParamsModel.__name__: OccamProvidedUserAgentParamsModel,
-    DefinedLLMAgentParamsModel.__name__: DefinedLLMAgentParamsModel,
+    AgentsChatParamsModel.__name__: AgentsChatParamsModel,
+    InvitedUserAgentParamsModel.__name__: InvitedUserAgentParamsModel,
 }
