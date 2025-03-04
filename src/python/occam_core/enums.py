389fb5a4e67debe4cd90c5c9f5b14af577a851f1
Mohamed Abdelbary <mo@occami.ai>
2025-03-04 23:10:38 +0000

    Update status as discussed and add notes from call

--- src/python/occam_core/enums.py
+++ src/python/occam_core/enums.py
@@ -2,12 +2,47 @@ from enum import Enum
 
 
 class ToolRunStatus(str, Enum):
-    PENDING = "PENDING"
+    """
+    new
+    ALIVE: 
+    BATCH_COMPLETED:
+    FAILED: we have batch number (with failure type)
+    RUNNING: we have batch number (with )
+    PAUSED: 
+
+    run sub-statuses
+    Big enum that overlaps across run statuses. Mainly relevant for failures and pauses.
+
+    Think of a number of classes/abstractions for failures and appropriate messages.
+    Create an abstraction that returns messages.
+
+    Can be children of AgentIOModel
+    - has run_result field
+    - request for modifications
+    - type of model (relates to topic of models)
+
+    Think of errors as AgentIOModel
+
+    Even AgentInstanceMetadata can be conceptualised as one of those messages
+
+    Move polling for status out of checkpointer.
+
+    Side but IMPORTANT note: Agent parameterisation and how it will work from Autogen.
+
+    User agent and multi-agent workspaces instantiation has some complexities in parameterisation:
+    - Budget and other "meta" parameters.
+    - 
+    """
+    ALIVE = "ALIVE"
     RUNNING = "RUNNING"
     PAUSED = "PAUSED"
-    COMPLETED = "COMPLETED"
+    BATCH_COMPLETED = "BATCH_COMPLETED"
     FAILED = "FAILED"
-    TERMINATED = "TERMINATED"
+
+
+class ToolRunSubStatus(str, Enum):
+    # TODO: Implement
+    ...
 
 
 # Aliasing tool run status as agent run status
