dd67811aa84a8aba042226fb2d201fe3e5fc09c7
amedhatm <ahmed@occam.ai>
2025-03-20 02:32:19 +0200

    update tool run states

--- src/python/occam_core/enums.py
+++ src/python/occam_core/enums.py
@@ -2,40 +2,15 @@ from enum import Enum
 
 
 class ToolRunStatus(str, Enum):
-    """
-    new
-    ALIVE:
-    BATCH_COMPLETED:
-    FAILED: we have batch number (with failure type)
-    RUNNING: we have batch number (with )
-    PAUSED:
-
-    run sub-statuses
-    Big enum that overlaps across run statuses. Mainly relevant for failures and pauses.
-
-    Think of a number of classes/abstractions for failures and appropriate messages.
-    Create an abstraction that returns messages.
-
-    Can be children of AgentIOModel
-    - has run_result field
-    - request for modifications
-    - type of model (relates to topic of models)
-
-    Think of errors as AgentIOModel
-
-    Even AgentInstanceMetadata can be conceptualised as one of those messages
-
-    Move polling for status out of checkpointer.
-
-    Side but IMPORTANT note: Agent parameterisation and how it will work from Autogen.
-
-    User agent and multi-agent workspaces instantiation has some complexities in parameterisation:
-    - Budget and other "meta" parameters.
-    -
-    """
     #These are states where the instance exists
     # but no run is active.
+    # alive means the python instance is live somewhere
     ALIVE = "ALIVE"
+    # sleeping means the db has all the instance info,
+    # but the instance itself is not live.
+    SLEEPING = "SLEEPING"
+    # batch completed means its alive, and in its last run
+    # has completed a batch.
     BATCH_COMPLETED = "BATCH_COMPLETED"
 
     # This is when a run is active.
