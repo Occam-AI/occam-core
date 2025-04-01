84dab4988bd410800e37242c3a868c00f4e0df1d
Mohamed Abdelbary <mo@occami.ai>
2025-04-01 19:04:37 +0200

    adding helper classes and api context

--- src/python/occam_core/util/data_types/occam.py
+++ src/python/occam_core/util/data_types/occam.py
@@ -2,14 +2,15 @@ import inspect
 import itertools
 import types
 import typing
-from typing import Any, Type, Union, get_origin
+from typing import Any, Optional, Type, Union, get_origin
+import uuid
 
 from occam_core.util.data_types.util import (recursive_model_convert,
                                              recursive_type_check,
                                              recursive_value_type_check)
 from occam_core.util.error import (StrictRequiredVariablesViolated,
                                    TypeCheckFailedException)
-from pydantic import BaseModel
+from pydantic import BaseModel, model_validator
 from typing_extensions import Self
 
 pydantic_class_name_label = 'pydantic_class_name'
@@ -321,3 +322,46 @@ class OccamDataType(BaseModel):
                         raise error
                     return False
         return True
+    
+
+
+class OccamUUID(uuid.UUID):
+    """
+    A subclass of uuid.UUID that ensures the UUID is a valid Occam UUID.
+    """
+
+    @staticmethod
+    def uuid4_no_dash():
+        return str(uuid.uuid4()).replace("-", "_")
+
+
+
+class ToolInstanceCoreContext(BaseModel):
+
+    # this is track the init instance
+    # for checkpointing and tracking.
+    instance_id: Optional[str] = OccamUUID.uuid4_no_dash()
+
+    # this is to track the channel
+    # in which tool activity ends up
+    session_id: Optional[str] = OccamUUID.uuid4_no_dash()
+
+    # ths is in case this is an agentic tool
+    # and it's being launched by agent
+    # agent_key. otherwise there's no
+    # way for us to tell who the agent is.
+    agent_key: Optional[str] = None
+
+    # this is the link for tracking the agent as it works.
+    run_link: Optional[str] = None
+
+    class Config:
+        extra = "forbid"
+
+    @model_validator(mode="after")
+    def check_instance_id(self):
+        if self.instance_id is None:
+            self.instance_id = OccamUUID.uuid4_no_dash()
+        if self.session_id is None:
+            self.session_id = OccamUUID.uuid4_no_dash()
+        return self
