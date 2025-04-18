6bd631afa965432f38ea3f7e8f0f6f044ac05c7b
amedhatm <ahmed@occam.ai>
2025-04-18 18:15:54 -0400

    update tool run status

--- src/python/occam_core/enums.py
+++ src/python/occam_core/enums.py
@@ -9,21 +9,26 @@ class ToolRunStatus(str, Enum):
     # sleeping means the db has all the instance info,
     # but the instance itself is not live.
     SLEEPING = "SLEEPING"
-    # batch completed means its alive, and in its last run
-    # has completed a batch.
-    BATCH_COMPLETED = "BATCH_COMPLETED"
-
-    # This is when a run is active.
-    RUNNING = "RUNNING"
 
     # When a pause has been requested, but not yet processed.
     PAUSE_REQUESTED = "PAUSE_REQUESTED"
+    RESUME_REQUESTED = "RESUME_REQUESTED"
+    TERMINATE_REQUESTED = "TERMINATE_REQUESTED"
+
+    # in progress signals
     PAUSE_IN_PROGRESS = "PAUSE_IN_PROGRESS"
+    RESUME_IN_PROGRESS = "RESUME_IN_PROGRESS"
+    TERMINATE_IN_PROGRESS = "TERMINATE_IN_PROGRESS"
 
     #These are states that we can resume from 
+    RUNNING = "RUNNING"
     PAUSED = "PAUSED"
     FAILED = "FAILED"
+    TERMINATED = "TERMINATED"
 
+    # batch completed means its alive, and in its last run
+    # has completed a batch.
+    BATCH_COMPLETED = "BATCH_COMPLETED"
 
 class ToolRunSubStatus(str, Enum):
     # TODO: Implement
