139fa4e5b5c65bdda244518001c993e2fd889379
amedhatm <ahmed@occam.ai>
2025-04-24 21:39:41 -0400

    only use base model instance variable after validation

--- src/python/occam_core/agents/util.py
+++ src/python/occam_core/agents/util.py
@@ -113,17 +113,17 @@ class TaggedAgentsModel(BaseModel):
 
     @model_validator(mode="after")
     def validate_tag_pairs(self):
-        self._tagged_agent_keys.clear()
-
+        _tagged_agent_keys = set()
         assert len(self.tag_models) > 0, "tag models must not be empty"
         for tag_model in self.tag_models:
             assert self.tagging_agent_key != tag_model.tagged_agent_key, \
                 f"tagging agent key must be different from tagged agent key: " \
                 f"tagging key: {self.tagging_agent_key} matches tagged key."
-            assert tag_model.tagged_agent_key not in self.tagged_agent_keys, \
+            assert tag_model.tagged_agent_key not in _tagged_agent_keys, \
                 f"tagged agent key must be unique. Got {tag_model.tagged_agent_key} " \
                 f"more than once."
-            self.tagged_agent_keys.add(tag_model.tagged_agent_key)
+            _tagged_agent_keys.add(tag_model.tagged_agent_key)
+        self.tagged_agent_keys = _tagged_agent_keys
 
         return self
 
