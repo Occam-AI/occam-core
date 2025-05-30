173060e0f1e73d5b6f99234c8b5f09574f4a3370
Mohamed Abdelbary <mo@occami.ai>
2025-05-30 16:04:43 +0100

    minor rename

--- src/python/occam_core/agents/util.py
+++ src/python/occam_core/agents/util.py
@@ -307,7 +307,7 @@ class EmailAttachmentMetadataModel(BaseModel):
     cc: Optional[list[str]] = None
     bcc: Optional[list[str]] = None
     # email attachments to send in the email.
-    attachments: Optional[list[FileMetadataModel]] = None
+    file_attachments: Optional[list[FileMetadataModel]] = None
 
     @model_validator(mode="after")
     def validate_attachment_id_required(self):
